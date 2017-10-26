7adcbd4c6a5084baa9f94f25d4ea2dcb0b38ea28
Rsandford1 <rsandford1@nait.ca>
2017-10-25 19:43:47 -0600

    MovingofSecurityClassesandRefactoring

---
+++ ClassDemo/Chinook.Data/Entities/Security/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region additionalnamespaces
+using Microsoft.AspNet.Identity.EntityFramework;
+#endregion
+
+namespace Chinook.Data.Entities.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
+    }
+
+}
